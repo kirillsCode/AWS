37a33228ce35d34177557a30a4ffc0bc78a4e110
kirillsCode <kirya.conf@mail.ru>
2018-02-21 05:45:06 +0700

    2 commit
    added http & tcp checking

--- test.py
+++ test.py
@@ -1,14 +1,56 @@
 import boto3
-#import httplib
-#import urllib
+import httplib
 import datetime
+import urllib2
 #from datetime import date
 from collections import defaultdict
+import socket
+
+hosts2 = ["ec2-52-33-204-20.us-west-2.compute.amazonaws.com", "ec2-54-69-249-106.us-west-2.compute.amazonaws.com", "ec2-34-217-203-77.us-west-2.compute.amazonaws.com"]
+hosts = ["ns1.scientia.pp.ua","ns2.scientia.pp.ua","ns3.scientia.pp.ua"]
+PORT = 22
+ips = []
+
+def tcp_conn(host):
+    print "Connecting to %s on port %s ..." % (host,PORT)
+    try:
+        s = socket.socket()
+        s.settimeout(3)
+        s.connect((host,PORT))
+        print "\tConnected"
+        s.close()
+        return False
+    except:
+        print "\tFailed to connect!!!"
+        return True
+
+def http_conn(host):
+    http = "http://"+host
+    print "Connecting by http ..."
+    try:
+        print urllib2.urlopen(http, None, 3).getcode()
+        return False
+    except:
+        print "\tFailed"
+        return True
+
+for h in hosts:
+    if tcp_conn(h) and http_conn(h):
+        try:
+            addr = socket.gethostbyname(h)
+            ips.append(addr)
+        except:
+            print "Couldn't resolve the host name!"
+    print "\n   ***   \n"
+    
+#ipss = ['52.33.204.20','54.69.249.106','34.217.203.77']
+#print ips
+#print ipss
 
 region = 'us-west-2'
 ec2=boto3.resource('ec2', region_name = region)
-dns = ['ec2-54-70-209-171.us-west-2.compute.amazonaws.com', 'ec2-34-217-218-93.us-west-2.compute.amazonaws.com', 'ec2-54-70-215-226.us-west-2.compute.amazonaws.com','ec2-52-33-204-20.us-west-2.compute.amazonaws.com']
-my_instances = ec2.instances.filter(Filters=[{'Name':'dns-name','Values':dns}])
+#dns = ['ec2-54-70-209-171.us-west-2.compute.amazonaws.com', 'ec2-34-217-218-93.us-west-2.compute.amazonaws.com', 'ec2-54-70-215-226.us-west-2.compute.amazonaws.com','ec2-52-33-204-20.us-west-2.compute.amazonaws.com']
+my_instances = ec2.instances.filter(Filters=[{'Name':'ip-address','Values':ips}])
 
 for inst in my_instances:
     instancename = ''
@@ -56,10 +98,8 @@ for inst_id, attrib in inst_attr.items():
         print("{:>20}: {}".format(key, attrib[key]))
     print("-----------------")
 
-#conn=httplib.HTTPConnection("scientia.pp.ua")
-#conn.request("HEAD", "/")
-#resp=conn.getresponse()
-#print resp.status, resp.reason
-	
-#print urllib.urlopen("http://scientia.pp.ua").getcode()
+#conn=httplib.HTTPConnection(host,80,timeout=5.0)
+ #       conn.request("HEAD", "/")
+  #      resp=conn.getresponse()
+   #     print "\t"+resp.status
 
